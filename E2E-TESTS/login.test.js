d2b9311408eecdb8e0f3341bbd952d805599925d
Alina Ghetler <alina.ghetler@gmail.com>
2020-10-03 21:27:50 +0200

    Add login test

--- E2E-TESTS/login.test.js
+++ E2E-TESTS/login.test.js
@@ -1,3 +1,12 @@
 import { Selector } from 'testcafe'
 
-fixture `The user should login`.page('http://localhost:8081/login')
+//prettier-ignore
+fixture`The user should login`
+    .page`http://localhost:8081/login`
+
+//positive test
+test('User should be able to login', async t => {
+    await t.typeText('.form-control', 'John')
+    await t.typeText('.form-control',)
+    await t.click(".btn btn-primary");
+})
